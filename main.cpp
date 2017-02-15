9fc846e7fd9b019ca793683d165f9412a211fb5b
Lea Douchet <lea.douchet@insa-lyon.fr>
2017-02-15 16:53:04 +0100

    main file

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//==============================
+//    INCLUDES
+//==============================
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "Cell.h"
+#include "Ga.h"
+#include "Gb.h"
+#include <vector>
+#include <list>
+
+//==============================
+//    FUNCTION DeCLARATION
+//==============================
+
+//==============================
+//    MAIN
+//==============================
+
+int main(int argc, char const *argv[])
+{
+	
+	
+	
+	}
