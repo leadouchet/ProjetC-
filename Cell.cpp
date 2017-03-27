29b9551c25c4863c14bec43b6452055deef29659
Florian Peron <florian.peron@insa-lyon.fr>
2017-03-27 14:35:41 +0200

    indentation Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,48 +1,55 @@
-//==============================
-//    INCLUDES
-//==============================
+//======================================================================
+//                            INCLUDES
+//======================================================================
 #include "Cell.h"
 
-//==============================
-//    DEFINITION STATIC ATTRIBUTES
-//==============================
-const float Cell::fitness_min_ = 0.001;
-const float Cell::Pdeath_ = 0.02; 
-const float Cell::Pmut_ = 0.001;
-const float Cell::dt_ = 0.1;
-int Cell::nb_cell = 0;
-//==============================
-//    CONSTRUCTORS
-//==============================
+//======================================================================
+//                  DEFINITION STATIC ATTRIBUTES
+//======================================================================
+  const float Cell::fitness_min_ = 0.001;
+  const float Cell::Pdeath_ = 0.02; 
+  const float Cell::Pmut_ = 0.001;
+  const float Cell::dt_ = 0.1;
+  int Cell::nb_cell = 0;
+  
+//======================================================================
+//                          CONSTRUCTORS
+//======================================================================
 Cell::Cell()
-{ fitness_ = 0;
-  concentrations_ = {0.0,0.0,0.0};
-  nb_cell += 1;
-}
+  { 
+    fitness_ = 0;
+    concentrations_ = {0.0,0.0,0.0};
+    nb_cell += 1;
+  }
 
-Cell::Cell(std::vector<float> intra_metabolites){
-  fitness_ = 0;
-  concentrations_= intra_metabolites;
-  nb_cell += 1;
-}
+Cell::Cell(std::vector<float> intra_metabolites)
+  {
+    fitness_ = 0;
+    concentrations_= intra_metabolites;
+    nb_cell += 1;
+  }
 
-//==============================
-//    DESTRUCTOR
-//==============================
-Cell::~Cell(){
+//======================================================================
+//                         DESTRUCTOR
+//======================================================================
+Cell::~Cell()
+  {
 	nb_cell-= 1;
-	}
-//==============================
-//    PUBLIC METHODS
-//==============================
-void Cell::Cell_division(){
-	concentrations_[0] /= 2;
-	concentrations_[1] /= 2;
-	concentrations_[2] /= 2;
-}
-//==============================
-//    GETTERS
-//==============================
+  }
+  
+//======================================================================
+//                        PUBLIC METHODS
+//======================================================================
+void Cell::Cell_division()
+  {
+    concentrations_[0] /= 2;
+    concentrations_[1] /= 2;
+    concentrations_[2] /= 2;
+  }
+  
+//======================================================================
+//                           GETTERS
+//======================================================================
 std::vector<float> Cell::intra_metabolites()
 {  
   return(concentrations_);
